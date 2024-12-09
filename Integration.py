b11c50584f249ae7cc08c7e5938b6793f1cc5488
Paul Florescu <paul.florescu22@imperial.ac.uk>
2024-12-09 10:19:39 +0000

    added more integation methods and a way of getting the time taken and error for each emthdo

--- Integration.py
+++ Integration.py
@@ -5,6 +5,7 @@ from scipy.special import erf
 import matplotlib.pyplot as plt
 import time
 
+
 # Define integrator class 
 
 class GaussianIntegrator:
@@ -76,93 +77,234 @@ class ExtendedTrapezium(GaussianIntegrator):
 
         return integral
     
+class Simpsons(GaussianIntegrator):
+
+    def integrate(self, func, a, b, num_points = 1000):
+        
+        trapezium = ExtendedTrapezium()
+        T_j = trapezium.integrate(func, a, b, num_points=num_points)
+
+        T_j_next = trapezium.integrate(func, a, b, num_points=2 * num_points)
+
+        S_j = (4 / 3) * T_j_next - (1 / 3) * T_j
+        return S_j
+    
 class MonteCarlo(GaussianIntegrator):
      
-    def integrate(self, func, a, b, num_samples=100):
+    def integrate(self, func, a, b, samples=10000):
 
-        random_samples = np.random.uniform(a, b, num_samples)
+        random_samples = np.random.uniform(a, b, samples)
     
-        function_values = func(random_samples)
+        func_values = func(random_samples)
         
-        integral_estimate = (b - a) * np.mean(function_values)
+        integral = (b - a) * np.mean(func_values)
         
-        return integral_estimate
+        return integral
     
 class ODE(GaussianIntegrator):
-    
-    def integrate(self, a, b):
-        pass
-        
-        
+   
+    def euler(self, func, a, b, steps = 1000):
+        h = (b - a) / steps
+        integral = 0  
+        x = a
 
+        for _ in range(steps):
+            integral += h * func(x) 
+            x += h  
+
+        return integral
+    
+    def rk4(self, func, a, b, steps = 1000):
+        h = (b - a) / steps
+        integral = 0  
+        x = a
+
+        for _ in range(steps):
+            k1 = func(x)
+            k2 = func(x + h / 2)
+            k3 = func(x + h / 2)
+            k4 = func(x + h)
+
+            integral += h * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+            x += h  
+        
+        return integral
     
 if __name__ == "__main__":
-     
-## Compare integration methods using a fixed h ##
+
+## Check Integration methods work ##
 
     erf_function = ErrorFunctionTest()
     trapezium  = ExtendedTrapezium()
     montecarlo = MonteCarlo()
+    ode = ODE()
 
-    a_values = np.linspace(0, 5, 500)  
+    erf_result = erf_function.integrate(a = 0, b = 5)
+    trapezium_result = trapezium.integrate(erf_function.gaussian, a = 0, b = 5)
+    montecarlo_result = montecarlo.integrate(erf_function.gaussian, a = 0, b = 5)
+    euler_result = ode.euler(erf_function.gaussian, a = 0,  b = 5)
+    rk4_result = ode.rk4(erf_function.gaussian, a = 0,  b = 5)
 
-    trapezium_results = []
-    trapezium_errors = []
-    trapezium_times = []
+    print(f"Actual result: {erf_result}")
+    print(f"Trapezium result: {trapezium_result}")
+    print(f"MonteCarlo result: {montecarlo_result}")
+    print(f"ODE Euler result: {euler_result}")
+    print(f"ODE RK4 result: {rk4_result}")
 
-    montecarlo_results = []
-    montecarlo_erros = []
+     
+## Compare integration methods on varying upper limit#
 
-    erf_results = []
-
+    if False:
     
-    
+        a_values = np.linspace(0, 5, 500)  
 
-    for a_value in a_values:
+        trapezium_results = []
+        trapezium_errors = []
+        trapezium_times = []
+
+        montecarlo_results = []
+        montecarlo_erros = []
+
+        erf_results = []
+
         
-        erf_result = erf_function.integrate(a = 0, b = a_value)
+        
+
+        for a_value in a_values:
+            
+            erf_result = erf_function.integrate(a = 0, b = a_value)
+        
+            trapezium_result = trapezium.integrate(erf_function.gaussian, a = 0, b = a_value)
+
+            montecarlo_result = montecarlo.integrate(erf_function.gaussian, a = 0, b = a_value)
+
+            erf_results.append(erf_result)
+            trapezium_results.append(trapezium_result)
+            montecarlo_results.append(montecarlo_result)
+
+            trapezium_errors.append(abs(erf_result - trapezium_result))
+            montecarlo_erros.append(abs(erf_result - montecarlo_result))
+
+
+        plt.scatter(a_values, trapezium_errors, label="Trapezium Error", marker="o")
+        #plt.scatter(a_values, montecarlo_erros, label="MonteCarlo Error", marker="x")
+        plt.xlabel("value of uppter limit")
+        plt.ylabel("Absolute Error")
+        plt.title("Integration Method Errors (Relative to ErrorFunction)")
+        plt.legend()
+        plt.grid()
+        plt.show()
+
+    ## Final Comparission ##
+
+    a, b = 0, 5
+
+    exact_int = ErrorFunctionTest()
+    exact_result = exact_int.integrate(a, b)
+    func = exact_int.gaussian
+
+    tolerance = 1e-9
+
+    trapezium_iterative = ExtendedTrapeziumIterative()
+    trapezium_set = ExtendedTrapezium()
+    simsons = Simpsons()
+    monte_carlo = MonteCarlo()
+    ode = ODE()
+
+    # Trapezium
+    steps = 2
+    start_time = time.time()
+    previous_result = 0 
+    while True:
        
-        trapezium_result = trapezium.integrate(erf_function.gaussian, a = 0, b = a_value)
+        result = trapezium_set.integrate(func, a, b, steps)
+        
+        error_exact = abs((exact_result - result) / exact_result) * 100
+        error = abs(result - previous_result) / abs(result)
 
-        montecarlo_result = montecarlo.integrate(erf_function.gaussian, a = 0, b = a_value)
+        previous_result = result
 
-        erf_results.append(erf_result)
-        trapezium_results.append(trapezium_result)
-        montecarlo_results.append(montecarlo_result)
-
-        trapezium_errors.append(abs(erf_result - trapezium_result))
-        montecarlo_erros.append(abs(erf_result - montecarlo_result))
-
-
-    plt.scatter(a_values, trapezium_errors, label="Trapezium Error", marker="o")
-    #plt.scatter(a_values, montecarlo_erros, label="MonteCarlo Error", marker="x")
-    plt.xlabel("value of uppter limit")
-    plt.ylabel("Absolute Error")
-    plt.title("Integration Method Errors (Relative to ErrorFunction)")
-    plt.legend()
-    plt.grid()
-    plt.show()
-
-    ## PLOT the time it takes vs the error for each method ##
-
-    tolerances = np.logspace(-8, -2, 1000)
-    trapezium_errors = []
-    trapezium_times = []
-
-    for tol in tolerances:
-        extended_trapezium = ExtendedTrapeziumIterative(tol = tol)
-
-        start_time = time.time()
-        trapezium_result = extended_trapezium.integrate(erf_function.gaussian, a=1, b=5)
-        trapezium_time = time.time() - start_time
-        trapezium_times.append(trapezium_time)
-
-        erf_result = erf_function.integrate(a=1 , b=5)
-        error = abs(erf_result - trapezium_result)
-        trapezium_errors.append(error)
-
-    plt.loglog(trapezium_times, trapezium_errors, label="Trapezium", marker="x")
-    plt.show()
-
+        if error < tolerance * 100:
+            break
+        steps += 1
     
+    time_taken = time.time() - start_time
+    print(f"Trapezium: Final Result = {result:.10f}, Error = {error_exact:.10f}%, Steps = {steps}, Time Taken = {time_taken:.6f}s")
 
+    # MonteCarlo
+    samples = 1
+    max_samples = 10000
+    start_time = time.time()
+    previous_result = 0
+    while samples < max_samples:
+       
+        result = monte_carlo.integrate(func, a, b, samples)
+        error_exact = abs((exact_result - result) / exact_result) * 100
+        error = abs(result - previous_result) / abs(result)
+
+        previous_result = result
+
+        if error < tolerance * 100:
+            break
+        samples += 1
+
+    time_taken = time.time() - start_time
+    print(f"MonteCarlo: Final Result = {result:.10f}, Error = {error:.10f}%, Samples = {samples}, Time Taken = {time_taken:.6f}s")
+    
+    # Euler
+    steps = 2
+    start_time = time.time()
+    previous_result = 0
+    while True:
+        start_time = time.time()
+        result = ode.euler(func, a, b, steps)
+        time_taken = time.time() - start_time
+        error_exact = abs((exact_result - result) / exact_result) * 100
+        error = abs(result - previous_result) / abs(result)
+
+        previous_result = result
+
+        if error < tolerance * 100:
+            break
+        steps += 1
+    
+    time_taken = time.time() - start_time
+    print(f"Euler: Final Result = {result:.10f}, Error = {error:.10f}%, Steps = {steps}, Time Taken = {time_taken:.6f}s")
+    
+    # rk4
+    start_time = time.time()
+    previous_result = 0
+    steps = 2
+    while True:
+        start_time = time.time()
+        result = ode.rk4(func, a, b, steps)
+        error_exact = abs((exact_result - result) / exact_result) * 100
+        error = abs(result - previous_result) / abs(result)
+
+        previous_result = result
+        if error < tolerance * 100:
+            break
+        steps += 1
+     
+    time_taken = time.time() - start_time
+    print(f"RK4: Final Result = {result:.10f}, Error = {error:.10f}%, Steps = {steps}, Time Taken = {time_taken:.6f}s")
+
+    # Simpsons
+    steps = 2
+    start_time = time.time()
+    previous_result = 0 
+    while True:
+       
+        result = simsons.integrate(func, a, b, steps)
+        
+        error_exact = abs((exact_result - result) / exact_result) * 100
+        error = abs(result - previous_result) / abs(result)
+
+        previous_result = result
+
+        if error < tolerance * 100:
+            break
+        steps += 1
+    
+    time_taken = time.time() - start_time
+    print(f"Simpsons: Final Result = {result:.10f}, Error = {error_exact:.10f}%, Steps = {steps}, Time Taken = {time_taken:.6f}s")
